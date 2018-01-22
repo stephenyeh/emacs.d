6f9b284a15acca947addb54276ae21c6003e0f33
yq <bahuasheng@gmail.com>
2018-01-21 22:41:16 -0800

    change default font

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -17,4 +17,6 @@
   (set-selection-coding-system (if (eq system-type 'windows-nt) 'utf-16-le 'utf-8))
   (prefer-coding-system 'utf-8))
 
+(set-default-font " -*-Menlo-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1")
+
 (provide 'init-locales)
